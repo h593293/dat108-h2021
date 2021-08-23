24e41c636e412d8396d02ae71a7af93c90884da3
Lars-Petter Helland <lars-petter.helland@hvl.no>
2021-08-23 11:08:37 +0200

    Eksempel fra F01

---
+++ F01 - Hallo Threads/src/no/hvl/dat108/f01/HalloMain.java
@@ -0,0 +1,51 @@
+package no.hvl.dat108.f01;
+
+public class HalloMain {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+/*
+ * Det er to hovedmåter (extends Thread og implements Runnable) 
+ * og 5 varianter i hvordan man kan opprette en tråd.
+ */
+		
+// #1	Thread t = new MinTraad();
+		
+// #2	Thread t = new Thread() {
+//			@Override
+//			public void run() {
+//				System.out.println("Hallo fra anonym Thread");
+//			}
+//		};
+		
+// #3	Thread t = new Thread(new MinRunnable());
+		
+// #4	Thread t = new Thread(new Runnable(){
+//			@Override
+//			public void run() {
+//				System.out.println("Hallo fra anonym Runnable");
+//			}
+//		});
+		
+// #5
+		Thread t = new Thread(() 
+				-> System.out.println("Hallo fra lambda-Runnable"));
+
+		t.start();
+		
+// ...
+
+//Starte mange tråder		
+		for (int i=0; i<10; i++) {
+			final int x = i;
+			Thread tt = new Thread(() -> System.out.println(
+					"Hallo fra tråd " + x));
+			tt.start();
+		}
+		
+		Thread.sleep(8);
+		
+		System.out.println("Hallo fra main");
+	}
+
+}
