c611ccf3e9c3437963c4e1e56ebfe11cd2bb6375
Lars-Petter Helland <lars-petter.helland@hvl.no>
2021-09-06 08:13:31 +0200

    Animert ball

---
+++ Bouncing Ball/src/animated/nothreads/AnimatedBalls.java
@@ -0,0 +1,41 @@
+package animated.nothreads;
+
+import static animated.nothreads.Constants.*;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class AnimatedBalls {
+
+	public static void main(String[] args) {
+		
+		//Her oppretter vi et ball-objekt med gitt farge og starthastighet
+		Ball roedBall = new Ball(Color.RED, randomVxVy(), randomVxVy());
+		
+		//Her oppretter vi en tegneflate og legger til ballobjektet
+		JPanel tegneflate = new JPanel();
+		tegneflate.setLayout(null);
+		tegneflate.add(roedBall);
+		
+		//Her oppretter vi selve 2D-grafikk-vinduet legger til tegneflaten
+		JFrame grafikkvinduet = new JFrame();
+		grafikkvinduet.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		grafikkvinduet.setLayout(new BorderLayout());
+		grafikkvinduet.setSize(PANE_SIZE, PANE_SIZE);
+		grafikkvinduet.add(tegneflate);
+		
+		//Her starter vi applikasjonen ved å gjøre grafikkvinduet synlig
+		//og be ballen om å gjøre animasjonen, dvs. bevege og tegne seg 
+		//på nytt i evig løkke.
+		grafikkvinduet.setVisible(true);
+		roedBall.animate();
+	}
+
+	private static int randomVxVy() {
+		return (int) Math.round((Math.random() * MAX_VX_VY));
+	}
+
+}
