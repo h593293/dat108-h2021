6799ba71be449636c3a7f2cffc160b3b78fa279f
Lars-Petter Helland <lars-petter.helland@hvl.no>
2021-09-13 11:19:57 +0200

    F07 - Løsninger fra timen

--- F06 - Intro til lambda-uttrykk/src/no/hvl/lph/dat108/f06/F06c.java
+++ F06 - Intro til lambda-uttrykk/src/no/hvl/lph/dat108/f06/F06c.java
@@ -10,7 +10,7 @@ interface Betingelse {
 
 public class F06c {
 	
-	public static List<Integer> filtrer(List<Integer> liste, Betingelse betingelse) {
+	public static List<Integer> plukkUt(List<Integer> liste, Betingelse betingelse) {
 		
 		List<Integer> resultat = new ArrayList<>();
 		
@@ -29,21 +29,21 @@ public class F06c {
 		List<Integer> resultat;
 		
 		//Resultat skal inneholde alle partall fra listen
-		resultat = filtrer(listen, x -> x % 2 == 0); 
+		resultat = plukkUt(listen, x -> x % 2 == 0); 
 		System.out.println(resultat);
 		
 		//Alle tall større enn 0
-		resultat = filtrer(listen, x -> x > 0); 
+		resultat = plukkUt(listen, x -> x > 0); 
 		System.out.println(resultat);
 		
 		//Alle tall større enn 5
-		resultat = filtrer(listen, x -> x > 5); 
+		resultat = plukkUt(listen, x -> x > 5); 
 		System.out.println(resultat);
 
 		//Alle tall større enn 7.
 		//Nei, her gjentar vi oss. Vi lager heller en metode storreEnn() 
 		//som returnerer et "dynamisk" lambda-uttrykk! :)
-		resultat = filtrer(listen, storreEnn(7)); 
+		resultat = plukkUt(listen, storreEnn(7)); 
 		System.out.println(resultat);
 	}
 	
