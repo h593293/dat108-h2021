0ede036e650abc1e7e201ee1d78c8c0b5b550f02
Lars-Petter Helland <lars-petter.helland@hvl.no>
2021-09-30 10:33:31 +0200

    F13 - Last ned før timen

---
+++ F13 - Last ned til timen/src/main/java/no/hvl/dat108/LogginnUtil.java
@@ -0,0 +1,13 @@
+package no.hvl.dat108;
+
+public class LogginnUtil {
+	
+	//Logge inn
+	
+	//Logge ut
+	
+	//Sjekke om request tilhører innlogget bruker
+	
+	//Annet ...?
+
+}
---
+++ F13 - Last ned til timen/src/main/java/no/hvl/dat108/Validator.java
@@ -0,0 +1,15 @@
+package no.hvl.dat108;
+
+public class Validator {
+
+    /**
+     * @param username Brukernavnet som skal valideres
+     * @return Om brukenavnet er gyldig eller ikke
+     *
+     * Med gyldig må jo her menes at brukeren er en bruker som
+     * har tilgang til å logge inn. ...
+     */
+    public static boolean isGyldigUsername(String username) {
+    	return false;
+    }
+}
