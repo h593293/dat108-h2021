38ee066cf6d799c179b2c00071cabcbeb12eed2b
Lars-Petter Helland <lars-petter.helland@hvl.no>
2021-09-30 11:50:46 +0200

    ....

--- F13 - Last ned til timen/src/main/java/no/hvl/dat108/Validator.java
+++ F13 - Last ned til timen/src/main/java/no/hvl/dat108/Validator.java
@@ -6,8 +6,9 @@ public class Validator {
      * @param username Brukernavnet som skal valideres
      * @return Om brukenavnet er gyldig eller ikke
      *
-     * Med gyldig må jo her menes at brukeren er en bruker som
-     * har tilgang til å logge inn. ...
+     * Et gyldig brukernavn består av 4 eller flere tegn. Lovlige tegn
+     * er bokstaver (små og store) inkl. de norske bokstavene, og tall.
+     * Brukernavnet kan ikke begynne med et tall.
      */
     public static boolean isGyldigUsername(String username) {
     	return false;
